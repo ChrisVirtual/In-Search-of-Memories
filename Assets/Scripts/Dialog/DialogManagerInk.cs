f3303cab0fa053af2705e38a937ae8956254b24a
Caleb-Cribb <ydr5224@autuni.ac.nz>
2024-04-12 00:51:57 +1200

    Implementing Dialog Aspect

---
+++ Assets/Scripts/Dialog/DialogManagerInk.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Ink.Runtime;
+using UnityEngine.EventSystems;
+
+public class DialogManagerInk : MonoBehaviour
+{
+    [Header("Dialog UI")]
+    [SerializeField] private GameObject dialogPanel;
+    [SerializeField] private TextMeshProUGUI dialogText;
+
+    private bool waitingForInput;
+    private Story currentStory;
+
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+
+    private TextMeshProUGUI[] choicesText;
+
+    public bool dialogIsPlaying { get; private set; }
+    public static DialogManagerInk instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Found more than one dialog manager");
+        }
+        instance = this;
+    }
+
+    void Start()
+    {
+        dialogIsPlaying = false;
+        dialogPanel.SetActive(false);
+
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0; 
+        
+        foreach(GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+    }
+
+    public void EnterDialogMode(TextAsset inkJSON)
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogIsPlaying = true;
+        dialogPanel.SetActive(true);
+        waitingForInput = true;
+        ContinueStory();
+    }
+
+    private void ExitDialogMode()
+    {
+        dialogIsPlaying = false;
+        dialogPanel.SetActive(false);
+        dialogText.text = "";
+    }
+
+    void Update()
+    {
+        if (!dialogIsPlaying)
+        {
+            return;
+        }
+
+        if (waitingForInput && Input.GetKeyDown(KeyCode.E))
+        {
+            ContinueStory();
+        }
+        else if (!waitingForInput && !currentStory.canContinue)
+        {
+            ExitDialogMode();
+        }
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogText.text = currentStory.Continue();
+            waitingForInput = true;
+
+            DisplayChoices();
+        }
+        else
+        {
+            waitingForInput = false;
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given than the UI can support");
+        }
+
+        int index = 0;
+
+        //Initialises choices to the amount of choices for this line of dialog
+        foreach (Choice choice in currentChoices) 
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+
+        //Hide leftover choices
+        for (int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(selectFirstChoice());
+    }
+
+    private IEnumerator selectFirstChoice()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+        
+    }
+
+    public void MakeChoice(int choiceIndex)
+    { 
+        currentStory.ChooseChoiceIndex(choiceIndex);
+    }
+
+
+
+}
---
+++ Assets/Scripts/Dialog/DialogTrigger.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float interactionRadius = 1f;
+
+    private void Awake()
+    {
+        visualCue.SetActive(false);
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+
+        if (distanceToPlayer <= interactionRadius)
+        {
+            visualCue.SetActive(true);
+            // You can trigger the dialogue here using inkJSON
+            if(!DialogManagerInk.instance.dialogIsPlaying) 
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    DialogManagerInk.instance.EnterDialogMode(inkJSON);
+                }
+            } 
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+    }
+
+    // This method can be used to set the player's transform when it enters the trigger zone
+    public void SetPlayerTransform(Transform player)
+    {
+        playerTransform = player;
+    }
+
+    // This method can be used to set the interaction radius
+    public void SetInteractionRadius(float radius)
+    {
+        interactionRadius = radius;
+    }
+
+    // Optionally, you can remove the OnTriggerEnter2D and OnTriggerExit2D methods
+}
