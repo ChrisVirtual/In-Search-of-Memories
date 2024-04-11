2c01669a23bd56ced502c3e4daa7b56d31aea4bf
Caleb-Cribb <ydr5224@autuni.ac.nz>
2024-04-12 00:50:32 +1200

    Squashed commit of the following:
    commit 0c5218873223da60b5cc5ed4f03bab646424a3a1
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 13:39:28 2024 +1200
    
        Real Commit
    
    commit f7091a38afa2db765c5d43a88f857787e898c36c
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 13:31:36 2024 +1200
    
        Update SampleScene.unity
    
    commit 581786aafee92273aa17cc5f5802a2cdb41e0046
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 13:29:52 2024 +1200
    
        mjhmjh
    
    commit 4bd3e988658064793dff9ba64944eec0643f83c8
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 13:11:39 2024 +1200
    
        Update .gitignore
    
    commit c3ed9ff1125126b932a0de61b0827dcb56fc7955
    Merge: 9f7da18 e872fb2
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 13:09:15 2024 +1200
    
        Merge pull request #2 from Christian-C-G-Jorge/Christian-Costa-Gomes-Jorge_Changes
    
        Christian costa gomes jorge changes
    
    commit e872fb29a4382cc2ad131acea5e1b3ba4d193721
    Merge: dfbeedf 9f7da18
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 13:06:15 2024 +1200
    
        Merge branch 'main' into Christian-Costa-Gomes-Jorge_Changes
    
    commit dfbeedf3b013982ab22daf19a3f8dbfbafe435f4
    Merge: a355ca8 c8dba91
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 13:01:07 2024 +1200
    
        Merge branch 'main' into Christian-Costa-Gomes-Jorge_Changes
    
    commit 9f7da18f22837d7bcf6477795aadb33f5ce2285a
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 12:56:10 2024 +1200
    
        Update .gitignore
    
    commit a355ca896ebdd45582db77491bdc5b568b9a40e3
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 12:52:05 2024 +1200
    
        update vscode
    
    commit 434811b7cd824943cf07a4966e7b61ef230baa29
    Author: Christian <74563603+Christian-C-G-Jorge@users.noreply.github.com>
    Date:   Tue Apr 9 12:48:12 2024 +1200
    
        0.2.1v
    
        - little  test scene
    
    commit 6c6d9dd6b8954d0d59c5007c530858d981a237df
    Author: Christian <christiankebjorge@gmail.com>
    Date:   Sun Apr 7 16:16:44 2024 +1200
    
        0.2v
    
         - World Palettes
         - Player Sprites
    
    commit 9cb04760770f4a7ca59ee40da5ccddce43248964
    Author: Christian <christiankebjorge@gmail.com>
    Date:   Fri Apr 5 16:44:01 2024 +1300
    
        0.1v
    
    commit c8dba91f51c802a06f13dc0365eb8517e65a9e94
    Author: Christian <christiankebjorge@gmail.com>
    Date:   Fri Apr 5 16:36:12 2024 +1300
    
        0.2.1v
    
        Fix of 0.2v
    
    commit 9eb3b7b7c4bb5ad632061d593c939abdc9987897
    Author: Christian <christiankebjorge@gmail.com>
    Date:   Fri Apr 5 16:31:55 2024 +1300
    
        Main 0.2v
    
         - Addition of player sprites
         - Addition of level 2 to 4 assets and palettes
         - Addition of level 2 to 4 object assets
    
    commit 4aa7c5dd771953d5328fc9342c6871a192994ba0
    Author: Christian <christiankebjorge@gmail.com>
    Date:   Tue Apr 2 17:18:47 2024 +1300
    
        Template Update #1
    
    commit 3df15894b6ff7f37f14b831a63df2aea91ec31b6
    Author: Christian <christiankebjorge@gmail.com>
    Date:   Tue Apr 2 16:44:44 2024 +1300
    
        Made Base Template

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    [SerializeField] GameObject dialogBox;
+    [SerializeField] Text dialogText;
+
+    [SerializeField] int lettersPerSecond; // allow to show on Unity
+
+    public event Action OnShowDialog;
+    public event Action OnHideDialog;
+
+    public static DialogManager Instance { get; private set; }
+
+    private void Awake() // This will expose Dialog Manager to the "world" allowing any class to be able to access this
+    {
+        Instance = this;
+    }
+
+    Dialog dialog;
+    int currentLine = 0;
+    bool isTyping;
+
+    public IEnumerator ShowDialog(Dialog dialog)
+    {
+        yield return new WaitForEndOfFrame(); // make a waiting time for the next frame be invoked
+        OnShowDialog?.Invoke();
+
+        this.dialog = dialog; // allow to call dialog variable multiple times
+        dialogBox.SetActive(true);
+        StartCoroutine(TypeDialog(dialog.Lines[0]));
+    }
+
+    public void HandleUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && !isTyping)
+        {
+            ++currentLine;
+            if (currentLine < dialog.Lines.Count) // If this is less than the total number of lines it will show the next line
+            {
+                StartCoroutine(TypeDialog(dialog.Lines[currentLine]));
+            }
+            else // It will turn off the dialogBox marking the end of the dialog
+            {
+                dialogBox.SetActive(false);
+                currentLine = 0; // it will reset the chat, which means the player can talk with the NPCs multiple times
+                OnHideDialog?.Invoke();
+            }
+        }
+    }
+
+    public IEnumerator TypeDialog(string line) // it will show a letter one by one like a person talking instead to just throw all the dialog at once
+    {
+        isTyping = true;
+        dialogText.text = ""; 
+        foreach (var letter in line.ToCharArray()) 
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+        isTyping = false;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public enum GameState { FreeRoam, Dialog, attacking, idle }
+public class GameController : MonoBehaviour
+{
+    [SerializeField] PlayerController playerController; //Serialized Field adds a new field when this scripr bet threw in Unity Inspector to add a player controller which means drag an object inside of it
+
+    GameState state;
+
+    private void Start() // Switch the state of the game from free roam to Dialog and vice versa
+    {
+        DialogManager.Instance.OnShowDialog += () =>
+        {
+            state = GameState.Dialog;
+        };
+        DialogManager.Instance.OnHideDialog += () =>
+        {
+            if (state == GameState.Dialog) 
+                state = GameState.FreeRoam;
+        };
+    }
+
+    private void Update() // Updates the game state of the game according with the situation
+    {
+        if (state == GameState.FreeRoam)
+        {
+            playerController.HandleUpdate();
+        }
+        else if (state == GameState.Dialog)
+        {
+            DialogManager.Instance.HandleUpdate();
+        }  
+        else if (state == GameState.attacking)
+        {
+            playerController.isAttacking = true;
+        }    
+    }
+}
