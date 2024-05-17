6dd579519bc3465a467185707a9f1e907caaf2ed
AlloysiusM <albrentmanlutac@gmail.com>
2024-05-17 17:01:02 +1200

    bug fixes
    fixing animation, weapon collision with health doesn't work but logic has been implemented

---
+++ Assets/Scripts/Weapon/AnimationEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public UnityEvent OnAnimationTriggered, OnAttackPerformed;
+    public void TriggerEvent()
+    {
+        OnAnimationTriggered?.Invoke();
+    }
+
+    public void TriggerAttack()
+    {
+        OnAttackPerformed?.Invoke();
+    }
+}
--- Assets/Scripts/Weapon/WeaponParent.cs
+++ Assets/Scripts/Weapon/WeaponParent.cs
@@ -14,6 +14,17 @@ public class WeaponParent : MonoBehaviour
     public float delay = 0.3f;
     private bool attackBlocked;
 
+    public Transform circleOrigin;
+    public float radius;
+    public LayerMask detectionLayerMask;
+
+    public bool isAttacking { get; private set; }
+
+    public void ResetIsAttacking()
+    {
+        isAttacking = false;
+    }
+
     public void Awake()
     {
         weaponTransform = transform;
@@ -22,6 +33,8 @@ public class WeaponParent : MonoBehaviour
 
     public void RotateTowards(Vector3 targetPosition)
     {
+        if (isAttacking)
+            return;
         Vector3 direction = (targetPosition - weaponTransform.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         weaponTransform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
@@ -37,17 +50,40 @@ public class WeaponParent : MonoBehaviour
         }
     }
 
-    public void Attack() {
-        if(attackBlocked) {
+    public void Attack()
+    {
+        if (attackBlocked)
+        {
             return;
         }
         animator.SetTrigger("Attack");
+        isAttacking = true;
         attackBlocked = true;
         StartCoroutine(DelayAttack());
     }
 
-    private IEnumerator DelayAttack() {
+    private IEnumerator DelayAttack()
+    {
         yield return new WaitForSeconds(delay);
         attackBlocked = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 positon = circleOrigin == null ? Vector3.zero : circleOrigin.position;
+        Gizmos.DrawWireSphere(positon, radius);
+    }
+
+    public void DetectColliders()
+    {
+        foreach (Collider2D collider in Physics2D.OverlapCircleAll(circleOrigin.position, radius))
+        {
+            Health health;
+            if (health = collider.GetComponent<Health>())
+            {
+                health.GetHit(10, transform.parent.gameObject);
+            }
+        }
     }
 }
