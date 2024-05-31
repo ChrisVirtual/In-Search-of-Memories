0dd213c397b9cdc4982b5e4682d6cad771b38452
AusaafMohammed17 <mohammedausaaf17@gmail.com>
2024-05-31 15:10:15 +1200

    Consumables Added
    Added consumables

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -5,7 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public void PlayGame()
+=======
+    [Header("Menu Buttons")]
+    [SerializeField] private Button newGameButton;
+    [SerializeField] private Button continueGameButton;
+
+    private void Start()
+    {
+        if (!DataPersistenceManager.instance.HasGameData())
+        {
+            continueGameButton.interactable = false;
+        }
+
+        // Add this to listen for scene changes
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        // Clean up listener to avoid memory leaks
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void NewGame()
+    {
+        DisableMenuButtons();
+        DataPersistenceManager.instance.NewGame();
+        SceneManager.LoadSceneAsync("Consumables");
+    }
+
+    public void LoadGame()
+    {
+        DisableMenuButtons();
+        SceneManager.LoadSceneAsync("Consumables");
+    }
+
+    public void SaveGame()
+>>>>>>> Stashed changes
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
